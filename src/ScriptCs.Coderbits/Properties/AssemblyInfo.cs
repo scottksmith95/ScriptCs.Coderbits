1e915a69af7cbdd21d0bbf9e918a2639db7c4e0d
Scott Smith <scottksmith95@gmail.com>
2013-05-14 12:30:43 -0700

    Updated version for NuGet

--- src/ScriptCs.Coderbits/Properties/AssemblyInfo.cs
+++ src/ScriptCs.Coderbits/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d172d3c2-89bd-4513-ac24-d66e46419f2b")]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
