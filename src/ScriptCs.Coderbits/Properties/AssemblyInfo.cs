a7ef9b57986cc4dd4097109040aa02dea30b5c87
Scott Smith <scottksmith95@gmail.com>
2013-05-22 07:33:53 -0700

    Updated dependencies

--- src/ScriptCs.Coderbits/Properties/AssemblyInfo.cs
+++ src/ScriptCs.Coderbits/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("d172d3c2-89bd-4513-ac24-d66e46419f2b")]
 
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
